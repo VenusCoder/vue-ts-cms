5ff879504fa5ecc2f0f3c728477180d44a6be34b
USER-Q0RUOJCHA6\Administrator <2549751987@qq.com>
2025-03-31 17:55:23 +0800

    第十次修改

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,10 +3,10 @@ import { localCache } from '@/tools/cache/cache'
 import { createRouter, createWebHistory } from 'vue-router'
 const Login = () => import('@/views/login/Login.vue')
 const Main = () => import('@/views/main/Main.vue')
-const DashBoard = () => import('@/views/main/analysis/dashboard/DashBoard.vue')
-const OverView = () => import('@/views/main/analysis/overview/OverView.vue')
-const Role = () => import('@/views/main/system/role/Role.vue')
-const User = () => import('@/views/main/system/user/User.vue')
+// const DashBoard = () => import('@/views/main/analysis/dashboard/DashBoard.vue')
+// const OverView = () => import('@/views/main/analysis/overview/OverView.vue')
+// const Role = () => import('@/views/main/system/role/Role.vue')
+// const User = () => import('@/views/main/system/user/User.vue')
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
--- src/stores/login/login.ts
+++ src/stores/login/login.ts
@@ -43,9 +43,15 @@ let useLoginStore = defineStore('login', {
       let usermenuresult: any = await getUserMenuRequest(this.userInfo.role.id)
       console.log(usermenuresult.data, '10000')
       this.userMenu = usermenuresult.data
-      const userMenu = this.userMenu
+      //这里有可能出错， 因为this.userMenu.data的data之前没加，是后来加的
+      const userMenu = this.userMenu.data
+            console.log(userMenu, 'userMenu')
 
-      //动态添加路由
+
+      //动态添加路由:
+      //需要注意的是，RouteRecordRaw 只是一个类型定义，它并不会自动注册路由。
+      // 要将其注册到 Vue Router 中，还需要使用 createRouter 函数创建路由实例，
+      // 并将路由记录传递给该实例的 routes 属性：
       const localRoutes: RouteRecordRaw[] = []
       const files: Record<string, any> = import.meta.glob('../../router/main/**/*.ts', {
         eager: true,
@@ -54,7 +60,16 @@ let useLoginStore = defineStore('login', {
 
       for (let key in files) {
         let module = files[key]
+        localRoutes.push(module.default)
         console.log(module.default, '77777')
+      }
+
+      for(let menu of userMenu){
+        for(let submenu of menu.children){
+          const route=localRoutes.find((item)=>item.path===submenu.url)
+        if(route)   router.addRoute('main',route)
+          
+        }
       }
 
       //保存用户userinfo本地缓存,注意token不能在这里缓存本地，因为获取role信息请求的id需要在携带token传进去
@@ -67,6 +82,7 @@ let useLoginStore = defineStore('login', {
 
       router.push('/main')
     },
+
   },
 })
 
